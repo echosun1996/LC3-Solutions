d68cf1042d00d0b94a2684faf3ee2de37415c8e3
Echo <echosun1996@126.com>
2025-05-05 15:48:52 +1000

    Update lab1.asm

--- lab1.asm
+++ lab1.asm
@@ -1,4 +1,4 @@
-.orig x3000
+.ORIG x3000
     ; R0    Temporary value of each result
     ; R1    X
     ; R2    Y
@@ -11,15 +11,15 @@
     LDR R1 , R3 , x0 ; R1 <- MEM[ x3100 + x0]
     LDR R2 , R3 , x1 ; R2 <- MEM[ x3100 + x1]
 
-    ; Compute the sum X +Y and place it at location x3102.
+    ; 1. Compute the sum X + Y and place it at location x3102.
     ADD R0, R1, R2
     STR R0, R3, x2 ; MEM[ x3100 + x2 ] <- R0
 
-    ; Compute X AND Y and place it at location x3103.
+    ; 2. Compute X AND Y and place it at location x3103.
     AND R0, R1, R2
     STR R0, R3, x3 ; MEM[ x3100 + x3 ] <- R0
 
-    ; Compute X OR Y and place it at location x3104.
+    ; 3. Compute X OR Y and place it at location x3104.
     ; De Morgan’s rule: X OR Y = NOT(NOT(X) AND NOT(Y)).
     NOT R4, R1  ; R4 ← NOT(R1)
     NOT R5, R2  ; R5 ← NOT(R2)
@@ -27,25 +27,25 @@
     NOT R0, R0  ; R0 ← NOT(R0)
     STR R0, R3, x4      ; MEM[ x3100 + x4 ] <- R0
 
-    ; Compute NOT(X) and place it at location x3105.
+    ; 4. Compute NOT(X) and place it at location x3105.
     NOT R0, R1  ; R0 ← NOT(X)
     STR R0, R3, x5      ; MEM[ x3100 + x5 ] <- R0
 
 
-    ; Compute NOT(Y) and place it at location x3106.
+    ; 5. Compute NOT(Y) and place it at location x3106.
     NOT R0, R2  ; R0 ← NOT(Y)
     STR R0, R3, x6      ; MEM[ x3100 + x6 ] <- R0
 
-    ; Compute X +3 and place it at location x3107.
+    ; 6. Compute X +3 and place it at location x3107.
     ADD R0, R1, #3
     STR R0, R3, x7      ; MEM[ x3100 + x7 ] <- R0
 
-    ; Compute Y −3 and place it at location x3108.
+    ; 7. Compute Y −3 and place it at location x3108.
     ADD R0, R2, #-3
     STR R0, R3, x8      ; MEM[ x3100 + x8 ] <- R0
 
-    ; If the X is even, place 0 at location x3109. If the number is odd, place 1 at the same location.
+    ; 8. If the X is even, place 0 at location x3109. If the number is odd, place 1 at the same location.
     AND R0, R1, #1      ; r3 = r0 & 1
     STR R0, R3, x9      ; MEM[ x3100 + x9 ] <- 0 or 1
 HALT
-.end
+.END
